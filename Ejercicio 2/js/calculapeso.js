ba5563c584e338272d1bf11b76ef7c1a35636d69
CERCATECH40-PC\Heidi Chaves <hchaves.learn@gmail.com>
2018-02-14 17:02:40 -0500

    Este ejercicio sencillo captura en pantalla un dato (peso de la persona en kilogramos) y arroja un resultado dependiendo de la opción seleccionada (planeta en la que le gustaría saber su peso).
    Dentro del HTML hay dos JavaScripts. Los dos hacen el mismo proceso, pero en uno se maneja la condición ifelse y en el otro se maneja la condición  switch.

---
+++ Ejercicio 2/js/calculapeso.js
@@ -0,0 +1,49 @@
+/* Heidi Chaves
+ * Ejemplo con IfElse */
+alert("funciona");
+
+var usuario = prompt("¿Dime, cual es tu peso en Kilogramos? ");
+var planeta = parseInt(prompt("Elije el planeta con el que quieras saber tu peso:\n1. = Mercurio. \n2. = Venus. \n3. = Marte. \n4. = Jupiter. \n5. = Saturno. \n6. = Urano. \n7. = Neptuno. "));
+var peso = parseFloat(usuario);
+var g_tierra = 9.8;
+var g_mercurio = 2.7;
+var g_venus = 8.8;
+var g_marte = 3.7;
+var g_jupiter = 22.8;
+var g_saturno = 9;
+var g_urano = 7.7;
+var g_neptuno = 11;
+var peso_final;
+var n_planeta;
+
+document.write("Tu peso en la tierra en kilogramos es de: <strong>" + peso + " kilogramos.</strong>" + "\n");
+
+if(planeta == 1){
+  peso_final = (peso * g_mercurio) / g_tierra;
+  n_planeta = "Mercurio";
+  //Error Nan: Significa not a number. javascript no ssabe q está tratando de hacer
+}else if(planeta == 2){
+  peso_final = (peso * g_venus) / g_tierra;
+  n_planeta = "Venus";
+}else if(planeta == 3){
+  peso_final = (peso * g_marte) / g_tierra;
+  n_planeta = "Marte";
+}else if(planeta == 4){
+  peso_final = (peso * g_jupiter) / g_tierra;
+  n_planeta = "Jupiter";
+}else if(planeta == 5){
+  peso_final = (peso * g_saturno) / g_tierra;
+  n_planeta = "Saturno";
+}else if(planeta == 6){
+  peso_final = (peso * g_urano) / g_tierra;
+  n_planeta = "Urano";
+}else if(planeta == 7){
+  peso_final = (peso * g_neptuno) / g_tierra;
+  n_planeta = "Neptuno";
+}else {
+  peso_final = 1000000;
+  document.write("Dato errado, por favor seleccione el valor solicitado. " + "\n");
+  planeta = "Kriptón";
+}
+peso_final = parseFloat(peso_final.toFixed(2));
+document.write("...Y el peso final en " + n_planeta + " es de <strong>: " + peso_final + " kilogramos.</strong>");
